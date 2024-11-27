79d685f489a90b573562e12a83febee2d7fad039
DANIEL OBENG <DOBENG1@JHU.EDU>
2024-11-27 16:16:44 -0500

    Add assertions and unit tests

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,60 @@
+#' Assert tbl Object Contains Specified Column(s)
+#'
+#' @param table_object tbl. A tbl in which to check for supplied column name(s).
+#' @param ... One or more column names (as strings).
+#'
+#' @return Silent return unless validation fail.
+assert_column_names_exist <- function(table_object, ...) {
+  browser()
+  dots <- list(...)
+  assertthat::assert_that(
+    all(purrr::map_chr(dots, class) == "character"),
+    msg = "At least one of the supplied values is not a string."
+  )
+
+  column_names <- unlist(dots)
+
+  assertthat::assert_that(
+    all(column_names %in% colnames(table_object)),
+    msg = glue::glue(
+      "At least one of the following columns does not exist in the supplied table object: \n
+      {paste(column_names, collapse = ' | ')}"
+    )
+  )
+}
+
+#' Assert Column in tbl Object is of a Specified Class
+#'
+#' @param table_object tbl. A tbl containing the column to validate.
+#' @param column_name string. The name of a column to be validated.
+#' @param column_class character vector. A vector of classes against which to validate.
+#'
+#' @return Silent return unless validation fails.
+assert_column_class <- function(table_object,
+                                column_name,
+                                column_class) {
+  assertthat::assert_that(
+    length(column_name) == 1,
+    msg = "Column class validation only works on one column at a time."
+  )
+
+  assert_column_names_exist(table_object, column_name)
+
+  column_class <- column_class %>%
+    tolower()
+
+  class_set <- table_object %>%
+    purrr::map(.f = class)
+
+  assertthat::assert_that(
+    any(class_set[[column_name]] %in% column_class),
+    msg = glue::glue(
+      "{column_name} does not possess one of the following classes: {paste(column_class,
+      collapse = ' | '}"
+    )
+  )
+}
+
 #' Estimate CATE for S-learner Using BART
 #'
 #' @param observed matrix. An array/matrix of posterior samples from
